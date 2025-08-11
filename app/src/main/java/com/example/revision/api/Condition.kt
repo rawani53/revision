f1dfdbf5c7ac443aa02f0e58b49c07e45a23e358
rawani53 <work.pranav53@gmail.com>
2025-08-11 14:26:25 +0530

    data classes and interface added

---
+++ app/src/main/java/com/example/revision/api/Condition.kt
@@ -0,0 +1,7 @@
+package com.example.revision.api
+
+data class Condition(
+    val code: String,
+    val icon: String,
+    val text: String
+)
---
+++ app/src/main/java/com/example/revision/api/WeatherModel.kt
@@ -0,0 +1,6 @@
+package com.example.revision.api
+
+data class WeatherModel(
+    val current: Current,
+    val location: Location
+)
